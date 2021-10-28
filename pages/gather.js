d26c1445e7d2eb44e2f9be2f79907c9c83cd85b6
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-10-28 03:34:57 +0200

    Add files via upload

---
+++ pages/gather.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('body')[0].innerHTML +=""+
+"<div id='box'>"+
+"<h1>Exploration</h1>"+
+"</div>";
---
+++ pages/shop.js
@@ -0,0 +1,17 @@
+document.getElementsByTagName('body')[0].innerHTML +=""+
+"<div id='box'>"+
+	"<h1>Shop</h1>"+
+		"<div id='stall'>"+
+			"<div class='case'><div id='obj1' class='obj'></div></div>"+
+			"<div class='case'><div id='obj2' class='obj'></div></div>"+
+			"<div class='case'><div id='obj3' class='obj'></div></div>"+
+			"<div class='case'><div id='obj4' class='obj'></div></div>"+
+			"<div class='case'><div id='obj5' class='obj'></div></div>"+
+			"<div class='case'><div id='obj6' class='obj'></div></div>"+
+			"<div class='case'><div id='obj7' class='obj'></div></div>"+
+			"<div class='case'><div id='obj8' class='obj'></div></div>"+
+			"<div class='case'><div id='obj9' class='obj'></div></div>"+
+	"</div>"+
+"</div>";
+
+showInv();

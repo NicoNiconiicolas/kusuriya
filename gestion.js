8bccb99082e7cc776ea224246e83232d0d5c4714
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-10-31 02:46:31 +0100

    Delete gestion.js

--- gestion.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByTagName('body')[0].innerHTML +=""+
-"<div id='box'>"+
-"<h1>Gestion</h1>"+
-"<h2>Url du deck utilisé :</h2>"+
-"<input id='deckLink' placeholder='"+getCookie('deck')+"'></input>"+
-"<br><br><div id='okButton' onclick='updateDeck()'>charger</div>"+
-"</div>";

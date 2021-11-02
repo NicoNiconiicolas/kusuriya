badcfb59d4488e3b2d7a398c2b6158b43912b59d
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-11-02 03:25:50 +0100

    Add files via upload

---
+++ model/menuEn.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('body')[0].innerHTML +=""+
+"<div id='footer'>"+
+"<div id='shop' class='menu-el' onclick='goto(\"shop\")'>shop</div>"+
+"<div id='synth' class='menu-el' onclick='goto(\"synth\")'>catalog</div>"+
+"<div id='exploration' class='menu-el' onclick='goto(\"gather\")'>exploration</div>"+
+"<div id='exploration' class='menu-el' onclick='goto(\"gestion\")'>management</div>"+
+"</div>";

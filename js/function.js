989019c2f23ac4093c295e92b744fa918ebcdaef
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-11-04 01:52:30 +0100

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1034,7 +1034,7 @@ function checkCards(){
 }
 
 function today(){
-	return new Date().getTime()/1000/60/60/24;
+	return Math.floor(new Date().getTime()/1000/60/60/24);
 }
 
 function setLang(lang){

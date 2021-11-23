3c25b4654cfc97569616a2f3a38962c2a7e54b1a
Kyuchama <35692364+kyuchama@users.noreply.github.com>
2021-11-23 16:58:59 +0100

    Add files via upload

--- pages/gestion.js
+++ pages/gestion.js
@@ -16,7 +16,8 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<h1>Gestion des decks</h1><br>"+
 "<input id='deckLink' placeholder='"+getCookie('deck')+"'></input>"+
 "　<div class='okButton' onclick='updateDeck()'>charger</div><br>"+
-"<br><div class='okButton' onclick='updateJLPT(\"N5\")'>N5</div> <div class='okButton' onclick='updateJLPT(\"N4\")'>N4</div> <div class='okButton' onclick='updateJLPT(\"N3\")'>N3</div> <div class='okButton' onclick='updateJLPT(\"N2\")'>N2</div> <div class='okButton' onclick='updateJLPT(\"N1\")'>N1</div>"+
+"<br><div class='okButton' onclick='updateJLPT(\"N5\")'>N5</div> <div class='okButton' onclick='updateJLPT(\"N4\")'>N4</div> <div class='okButton' onclick='updateJLPT(\"N3\")'>N3</div> <div class='okButton' onclick='updateJLPT(\"N2\")'>N2</div> <div class='okButton' onclick='updateJLPT(\"N1\")'>N1</div><br><br>"+
+"<div class='okButton' onclick='updateJLPT(\"hiragana\")'>hiragana</div> <div class='okButton' onclick='updateJLPT(\"katakana\")'>katakana</div> <div class='okButton' onclick='updateJLPT(\"kana\")'>kana</div>"+
 "<br><br><div class='okButton' id='deck1' onclick='chooseCookieDeck(1)'>"+getCookie('deck1_name')+"</div> <div class='okButton' id='deck2' onclick='chooseCookieDeck(2)'>"+getCookie('deck2_name')+"</div> <div class='okButton' id='deck3' onclick='chooseCookieDeck(3)'>"+getCookie('deck3_name')+"</div>"+
 "<br><br><div class='okButton' onclick='window.location=\"convert.html\"'>Nouveau Deck</div>"+
 "<br><br><h1>Options</h1>"+

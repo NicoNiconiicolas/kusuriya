d4b3bad9e79d03587d2002a699028d921343f935
Kyu <35692364+budouen@users.noreply.github.com>
2022-01-23 01:41:08 +0100

    Add files via upload

--- model/menu.js
+++ model/menu.js
@@ -1,7 +1,5 @@
 document.getElementsByTagName('body')[0].innerHTML +=""+
 "<div id='footer'>"+
-"<div id='shop' class='menu-el' onclick='goto(\"shop\")'>boutique</div>"+
-"<div id='synth' class='menu-el' onclick='goto(\"synth\")'>catalogue</div>"+
 "<div id='exploration' class='menu-el' onclick='goto(\"gather\")'>exploration</div>"+
 "<div id='exploration' class='menu-el' onclick='goto(\"gestion\")'>gestion</div>"+
 "</div>";

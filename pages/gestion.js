dc817ead147135037c8d352c550a4d5c76297652
Kyu <35692364+budouen@users.noreply.github.com>
2022-03-20 23:39:01 +0100

    Add files via upload

--- pages/gestion.js
+++ pages/gestion.js
@@ -6,7 +6,8 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<br><div class='okButton' title='Kotoba N5' onclick='updateJLPT(\"N5\")'>N5</div> <div class='okButton' title='Kotoba N4' onclick='updateJLPT(\"N4\")'>N4</div> <div class='okButton' title='Kotoba N3' onclick='updateJLPT(\"N3\")'>N3</div> <div class='okButton' title='Kotoba N2' onclick='updateJLPT(\"N2\")'>N2</div> <div class='okButton' title='Kotoba N1' onclick='updateJLPT(\"N1\")'>N1</div><br><br>"+
 "<div class='okButton' onclick='updateJLPT(\"hiragana\")'>hiragana</div> <div class='okButton' onclick='updateJLPT(\"katakana\")'>katakana</div> <div class='okButton' title='hiragana + katakana' onclick='updateJLPT(\"kana\")'>kana</div>"+
 "<br><br><div class='okButton' title='glisser-déposer un deck créé avec Cartory' onclick='goto(\"gather&dd=y\")'>glisser-déposer</div><br>"+
-"<br><div class='okButton' title='app pour création & gestion de decks' onclick='window.open(`cartory/index.html`,`_blank`)'>Cartory</div><br>"+
+"<br><br><div class='okButton' title='convertir un deck au format Cartory/Cardogus' onclick='window.location=`convert.html`'>convetisseur</div>"+
+"<br><br><div class='okButton' title='app pour création & gestion de decks' onclick='window.open(`cartory/index.html`,`_blank`)'>Cartory</div><br>"+
 "<br><br><h1>Options</h1>"+
 "<h3>Style de révision :</h3>"+
 "<div id='kotoba' class='okButton' onclick='chooseGame(1)'>Kotoba</div> <div id='anki' class='okButton' onclick='chooseGame(2)'>Anki</div>"+

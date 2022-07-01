db64795daa512171bf9943d6563371d127d606fe
Kyu <35692364+kyuhubb@users.noreply.github.com>
2022-07-01 14:55:12 +0200

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -2,15 +2,55 @@ let failed = 'nope';
 let achieved = false;
 
 function goto(page){
-	window.open('cartory/index.html', '_blank');
+	location.href='index.html?p='+page;
 }
 
 function bar(perc){
 	document.getElementById('progress').style.width = perc+"%";
 }
 
+function updateFulldeck(bool){
+	if(bool == 1){
+		// full deck random
+		setCookie('fulldeck', 'true', 3650);
+		setCookie('randomness', 'true', 3650);
+		document.getElementById('fullrandom').style.color = 'steelblue';
+		document.getElementById('fullordered').style.color = 'lightgrey';
+		document.getElementById('random').style.color = 'lightgrey';
+		document.getElementById('startid').style.display = 'none';
+		setCookie("startnum", '1', 3650);
+	}else if(bool == 2){
+		// full deck ordre
+		setCookie('fulldeck', 'true', 3650);
+		setCookie('randomness', 'false', 3650);
+		setCookie("startnum", '1', 3650);
+		document.getElementById('fullrandom').style.color = 'lightgrey';
+		document.getElementById('fullordered').style.color = 'steelblue';
+		document.getElementById('random').style.color = 'lightgrey';
+		document.getElementById('inputid').placeholder = getCookie('startnum');
+		document.getElementById('startid').style.display = 'inline';
+	}else{
+		// 15 random
+		setCookie('fulldeck', 'false', 3650);
+		setCookie('randomness', 'true', 3650);
+		setCookie("startnum", '1', 3650);
+		document.getElementById('fullrandom').style.color = 'lightgrey';
+		document.getElementById('fullordered').style.color = 'lightgrey';
+		document.getElementById('random').style.color = 'steelblue';
+		document.getElementById('startid').style.display = 'none';
+	}
+}
+
+function getTime(){
+	return Math.round(new Date().getTime()/1000/60);
+}
+
 function initQuestion(fail){
 	diviseur = 100/quest.length;
+	if(getCookie('fulldeck') == 'true' && getCookie('randomness') == 'false'){
+		progression = diviseur * parseInt(getCookie('startnum'))-1;
+		bar(progression);
+	}
 	if(fail == false){
 		lastQ2 = questions[questions.length-1]
 		lastA2 = answers[answers.length-1]
@@ -27,15 +67,39 @@ function initQuestion(fail){
 	for (i = 0; i < max; ++i){
     	values.push(i);
 	}
-	
-	var numberofquestions = max;
-		for(i = 0; i < numberofquestions; ++i){
-		var n = values.splice(Math.random()*values.length,1)[0];
-		questions.push(quest[n]);
-		answers.push(ans[n]);
-		recomment.push(comment[n]);
-		nQuestion = 1;
+	if(getCookie('fulldeck') == 'false'){
+		var numberofquestions = 15;
+	}else{
+		var numberofquestions = max;
 	}
+
+	if(getCookie('randomness') == 'false'){
+		answers = ans;
+		questions = quest;
+		recomment = comment;
+		numberofquestions = max;
+		if(achieved == true){
+			nQuestion = 0;
+			document.getElementById('goodAnswer').innerHTML = 'n/a';
+			document.getElementById('goodAnswer').onclick = '#';
+			document.getElementById('goodAnswer').style.color = 'ghostwhite';
+		}else{
+			nQuestion = parseInt(getCookie('startnum'))-1;
+		}
+	}else{
+		for(i = 0; i < numberofquestions; ++i){
+			var n = values.splice(Math.random()*values.length,1)[0];
+			questions.push(quest[n]);
+			answers.push(ans[n]);
+			recomment.push(comment[n]);
+			if(getCookie('randomness') == 'false'){
+				nQuestion = parseInt(getCookie('startnum'))-1;
+			}else{
+				nQuestion = 1;
+			}
+		}
+	}
+	setCookie('startnum', 0, 3650);
 	newQuestion();
 }
 
@@ -96,9 +160,9 @@ function checkAnswer(){
 	if(correct == true){
 		totalseen++;
 		totalcorrect++;
-		document.getElementById('goodAnswer').style.color = 'deepskyblue';
+		document.getElementById('goodAnswer').style.color = 'steelblue';
 		document.getElementById('correct').play();
-		progression = progression+diviseur;
+		if(getCookie('fulldeck') == 'true'){progression = progression+diviseur;}
 		bar(progression);
 	}else{
 		totalseen++;
@@ -116,45 +180,13 @@ function checkAnswer(){
 	if(recomment[nQuestion] != undefined){
 		document.getElementById('compar').innerHTML = recomment[nQuestion].replace(/\n/g, '<br>');
 	}
-	document.getElementById('goodAnswer').onclick = function(){window.open('https://jisho.org/search/'+questions[nQuestion-1], '_blank');};
+	if(getCookie('dico') == "jisho"){
+		document.getElementById('goodAnswer').onclick = function(){window.open('https://jisho.org/search/'+questions[nQuestion-1], '_blank');};
+	}else{
+		document.getElementById('goodAnswer').onclick = function(){window.open('https://dictionary.writtenchinese.com/#sk='+questions[nQuestion-1]+'&svt=pinyin', '_blank');};
+	}
 	nQuestion ++;
 	newQuestion();
-}
-
-function showmemo(){
-	var show = '';
-	for(x = 0 ; memo.length > x ; x++){
-		show += memo[x]+'<br>';
-	}
-	document.getElementById('memocont').innerHTML = show;
-	openmemo();
-}
-
-function addmemo(){
-	var exist = false;
-		for(x = 0 ; x <= memo.length ; x++){
-			var prememo = questions[nQuestion-1];
-			if(prememo == ""){
-				return
-			}
-
-			if(memo[x] == prememo){
-				exist = true;
-			}
-		}
-		if(exist != true){
-		memo.push(prememo);				
-	}
-}
-
-function closememo(){
-	document.getElementById('memo').style.display = 'none';
-	document.getElementById('combo').style.display = 'block';
-}
-
-function openmemo(){
-	document.getElementById('memo').style.display = 'block';
-	document.getElementById('combo').style.display = 'none';
 }
 
 function failedInit(){
@@ -193,11 +225,13 @@ function failedInit(){
 		failed = 'failed';
 		newQuestion();
 	}else{
-		alert('Deck is restarting...');
-		progression = 0;
-		bar(progression);
-		achieved = true;
-		failed = 'nope';
+		if(getCookie('fulldeck') == 'true'){
+			alert('Deck is restarting...');
+			progression = 0;
+			bar(progression);
+			achieved = true;
+			failed = 'nope';
+		}
 		if(wasWrong == true){
 			initQuestion(true);
 			wasWrong = false;	
@@ -207,8 +241,171 @@ function failedInit(){
 	}
 }
 
+
+function showmemo(){
+	var show = '';
+	for(x = 0 ; memo.length > x ; x++){
+		show += memo[x]+'<br>';
+	}
+	document.getElementById('memocont').innerHTML = show;
+	openmemo();
+}
+
+function addmemo(){
+	var exist = false;
+		for(x = 0 ; x <= memo.length ; x++){
+			var prememo = questions[nQuestion-1];
+			if(prememo == ""){
+				return
+			}
+
+			if(memo[x] == prememo){
+				exist = true;
+			}
+		}
+		if(exist != true){
+		memo.push(prememo);				
+	}
+}
+
+function updateDeck(){
+	var url = document.getElementById('deckLink').value;
+	if(url == ''){
+		if(lang == 0){
+			alert('empty field');
+		}else{
+			alert('champ vide');
+		}
+	}else{
+		setCookie('deck', url, 3650);
+		document.getElementById('deckLink').value = '';
+		document.getElementById('deckLink').placeholder = url;
+	}
+}
+
+function updateJLPT(n){
+	setCookie('deck', 'deck/'+n+'.js', 3650);
+	document.getElementById('deckLink').placeholder = "deck/"+n+".js";
+}
+
 function upcolor(){
 	this.style.color = 'purple';
+}
+
+function setLang(lang){
+	setCookie('lang' , lang, 3650);
+	window.location = 'index.html';
+}
+
+function jeu2(){
+	document.getElementById('showans').innerHTML = answers[nQuestion];
+	if(getCookie('dico') == "jisho"){
+		document.getElementById('showans').onclick = function(){window.open('https://jisho.org/search/'+questions[nQuestion], '_blank');};
+	}else{
+		document.getElementById('showans').onclick = function(){window.open('https://dictionary.writtenchinese.com/#sk='+questions[nQuestion]+'&svt=pinyin', '_blank');};
+	}
+	document.getElementById('showans').style.cursor = 'pointer';
+	document.getElementById('showans').onmouseover = function(){document.getElementById('showans').style.background = '#EEE'};
+	document.getElementById('showans').onmouseout = function(){document.getElementById('showans').style.background = 'white'};
+	document.getElementById('answerbox2').style.display = "none";
+	document.getElementsByClassName('ansbu')[0].style.display = "block";
+	document.getElementsByClassName('ansbu')[1].style.display = "inline-block";
+	document.getElementsByClassName('ansbu')[2].style.display = "inline-block";
+}
+
+function jeu2check(num){
+	if(num == 1){
+		document.getElementById('answer').value = answers[nQuestion].split(',')[0];
+	}else{
+		document.getElementById('answer').innerHTML = "";
+	}	
+		document.getElementById('showans').style.background = 'white';
+		document.getElementById('showans').onmouseover = '';
+		document.getElementById('showans').onmouseout = '';
+		document.getElementById('showans').style.cursor = 'default';
+		document.getElementById('showans').onclick = "";
+		document.getElementById('showans').innerHTML = "? ? ?";
+		document.getElementById('answerbox2').style.display = "inline-block";
+		document.getElementsByClassName('ansbu')[0].style.display = "none";
+		document.getElementsByClassName('ansbu')[1].style.display = "none";
+		document.getElementsByClassName('ansbu')[2].style.display = "none";
+		checkAnswer();
+}
+
+function chooseGame(num){
+	setCookie('jeu', num, 3650);
+	if(num == 1){
+		document.getElementById('kotoba').style.color = 'steelblue';
+		document.getElementById('anki').style.color = 'lightgrey';
+	}else{
+		document.getElementById('anki').style.color = 'steelblue';
+		document.getElementById('kotoba').style.color = 'lightgrey';
+	}
+}
+
+function setDico(dico){
+	if(dico == "jisho"){
+		setCookie('dico', "jisho", 3650);
+		document.getElementById('dicojisho').style.color = 'steelblue';
+		document.getElementById('dicochinese').style.color = 'lightgrey';
+	}else{
+		setCookie('dico', "writtenchinese", 3650);
+		document.getElementById('dicojisho').style.color = 'lightgrey';
+		document.getElementById('dicochinese').style.color = 'steelblue';
+	}
+}
+
+function checkrandomness(){
+	if(getCookie('randomness') == 'false'){
+		document.getElementById('cardId').style.display = 'block';
+	}
+}
+
+function checkrandomnessmenu(){
+	if(getCookie('randomness') == 'false'){
+		document.getElementById('startid').style.display = 'inline';
+	}
+}
+
+function choosestart(){
+	var number = document.getElementById('inputid').value;
+	setCookie("startnum", number, 3650);
+	document.getElementById('inputid').placeholder = getCookie('startnum');
+	document.getElementById('inputid').value = '';
+}
+
+function checkcolors(){
+	if(getCookie('jeu') == '1'){
+		document.getElementById('kotoba').style.color = 'steelblue';
+		document.getElementById('anki').style.color = 'lightgrey';	
+	}else{
+		document.getElementById('kotoba').style.color = 'lightgrey';
+		document.getElementById('anki').style.color = 'steelblue';
+	}
+
+	if(getCookie('dico') == 'jisho'){
+		document.getElementById('dicojisho').style.color = 'steelblue';
+		document.getElementById('dicochinese').style.color = 'lightgrey';
+	}else{
+		document.getElementById('dicojisho').style.color = 'lightgrey';
+		document.getElementById('dicochinese').style.color = 'steelblue';
+	}
+
+	if(getCookie('fulldeck') == 'true'){
+		if(getCookie('randomness') == 'true'){
+			document.getElementById('fullrandom').style.color = 'steelblue';
+			document.getElementById('fullordered').style.color = 'lightgrey';
+			document.getElementById('random').style.color = 'lightgrey';
+		}else{
+			document.getElementById('fullrandom').style.color = 'lightgrey';
+			document.getElementById('fullordered').style.color = 'steelblue';
+			document.getElementById('random').style.color = 'lightgrey';
+		}
+	}else{
+		document.getElementById('fullrandom').style.color = 'lightgrey';
+		document.getElementById('fullordered').style.color = 'lightgrey';
+		document.getElementById('random').style.color = 'steelblue';
+	}
 }
 
 function closecom(){
@@ -220,3 +417,44 @@ function opencom(){
 	document.getElementById('com').style.display = 'block';
 	document.getElementById('combo').style.display = 'none';
 }
+
+function closememo(){
+	document.getElementById('memo').style.display = 'none';
+	document.getElementById('combo').style.display = 'block';
+}
+
+function openmemo(){
+	document.getElementById('memo').style.display = 'block';
+	document.getElementById('combo').style.display = 'none';
+}
+
+function altgame(){
+	if(getCookie('jeu') == '1'){
+	  document.getElementById('answerbox').style.display = 'none';
+	  document.getElementById('goodAnswer').style.display = 'none';
+	  document.getElementsByClassName('ansbox')[0].style.display = 'block';
+	  document.getElementsByClassName('ansbox')[1].style.display = 'inline-block';
+	  document.getElementsByClassName('ansbox')[2].style.display = 'block';
+	  document.getElementById('answerbox2').style.display = 'inline-block';
+	  document.getElementById('answer').value = '';
+	  setCookie('jeu', '0', 3650);
+	}else{
+  		document.getElementsByClassName('ansbox')[0].style.display = 'none';
+  		document.getElementsByClassName('ansbox')[1].style.display = 'none';
+  		document.getElementsByClassName('ansbox')[2].style.display = 'none';
+  		document.getElementById('answerbox2').style.display = 'none';
+  		document.getElementById('ansbu2').style.display = 'none';
+  		document.getElementById('ansbu1').style.display = 'none';
+  		document.getElementById('answerbox').style.display = 'block';
+  		document.getElementById('goodAnswer').style.display = 'inline-block';
+  		setCookie('jeu', '1', 3650);
+  		document.getElementById('answer').value = '';
+	}
+}
+
+
+// if(progression > 100){
+// 	bar(99.4);
+// }else{
+// 	bar(progression);
+// }
--- pages/gather.js
+++ pages/gather.js
@@ -7,13 +7,23 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<div id='com'><h1 id='comtitle'>コメント</h1><button onclick='closecom()'>X</button><br><span id='compar'></span><br><br></div>"+
 "<div id='memo'><h1>メモ</h1><button onclick='closememo()'>X</button><br><br><br><span id='memocont'></span></div>"+
 "<div id='cardId'>#<span id='cid'>1</span></div>"+
-"<div id='question'>n/a</div>"+
+"<div id='question' onclick='altgame()'>n/a</div>"+
 "<div id='answerbox'><input id='answer' autofocus></input></div>"+
+"<br class='ansbox'><div class='ansbox' id='showans'>? ? ?</div>"+
+"<br class='ansbox'><div id='answerbox2' onclick='jeu2()'>show answer</div>"+
 "<br class='ansbu'><div class='ansbu' id='ansbu1' onclick='jeu2check(1)'>correct</div><div class='ansbu' id='ansbu2' onclick='jeu2check(2)'>incorrect</div>"+
 "<div id='goodAnswer'>n/a</div>"+
 "<div id='combo'><span onclick='addmemo()' class='minmenu'>「ajouter au memo」</span><span onclick='showmemo()' class='minmenu'>「voir memo」</span><span class='minmenu' onclick='opencom()'>「commentaires」</span></div>";
-document.getElementById('answerbox').style.display = 'block';
-document.getElementById('goodAnswer').style.display = 'inline-block';
+if(getCookie('jeu') == '1'){
+  document.getElementById('answerbox').style.display = 'block';
+  document.getElementById('goodAnswer').style.display = 'inline-block';
+}else{
+  document.getElementsByClassName('ansbox')[0].style.display = 'block';
+  document.getElementsByClassName('ansbox')[1].style.display = 'inline-block';
+  document.getElementsByClassName('ansbox')[2].style.display = 'block';
+  document.getElementById('answerbox2').style.display = 'inline-block';
+}
+
 var totalseen = 0;
 var totalcorrect = 0;
 var final = [];
@@ -23,9 +33,18 @@ var ans = [];
 var answers = [];
 var comment = [];
 var recomment = [];
-setTimeout(function(){document.getElementById('file-input').addEventListener('change', readSingleFile, false)}, 100);
+var progression = 0;
 
-document.getElementsByTagName('label')[0].style.display = 'block';
+if($_GET('dd') != 'y'){
+   var deck = document.createElement('script');
+   deck.src = getCookie('deck');
+   if(getCookie('fulldeck') == 'true'){
+      document.getElementById('progressBar').style.display = 'block';
+    }
+}else{
+  document.getElementsByTagName('label')[0].style.display = 'block';
+  setTimeout(function(){document.getElementById('file-input').addEventListener('change', readSingleFile, false)}, 100);
+}
 let nQuestion = 0;
 let lastQ = "";
 let lastA = "";
@@ -38,7 +57,7 @@ let wrongQ = [];
 let wrongC = [];
 let wasWrong = false;
 let memo = [];
-var progression = 0;
+checkrandomness();
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "Enter"){
@@ -46,19 +65,24 @@ document.addEventListener("keydown", (e) => {
   	document.getElementById('answer').value = "";
   }
 });
+if($_GET('dd') != 'y'){
+  document.getElementsByTagName('body')[0].appendChild(deck);
+}
 
 function readSingleFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-     return;
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var contents = e.target.result;
+      eval(contents);
+    };
+    reader.readAsText(file);
+    null;
+    document.getElementsByTagName('label')[0].style.display = 'none';
+    if(getCookie('fulldeck') == 'true'){
+      document.getElementById('progressBar').style.display = 'block';
+    }
   }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    eval(contents);
-  };
-  reader.readAsText(file);
-  null;
-  document.getElementsByTagName('label')[0].style.display = 'none';
-  document.getElementById('progressBar').style.display = 'block';
-}

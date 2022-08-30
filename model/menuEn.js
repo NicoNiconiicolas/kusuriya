5c77332572505c2074bbf77f5f88c2626b476b43
Kyu <35692364+kyuhubb@users.noreply.github.com>
2022-08-31 00:27:03 +0200

    Delete model directory

--- model/menuEn.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByTagName('body')[0].innerHTML +=""+
-"<div id='footer'>"+
-"<div id='exploration' class='menu-el' onclick='goto(\"gather\")'>deck</div>"+
-"<div id='gestion' class='menu-el' onclick='goto(\"gestion\")'>management</div>"+
-"</div>";

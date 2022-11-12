16e55e45e591392a404d5a6a84ae13809e435059
Kyu <35692364+kyuhubb@users.noreply.github.com>
2022-11-12 17:36:35 +0100

    Add files via upload

--- pages/gather.js
+++ pages/gather.js
@@ -5,7 +5,7 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<audio id='wrong' src='sfx/wrong.mp3'></audio>"+
 "<audio id='click' src='sfx/click.mp3'></audio>"+
 "<div id='progressBar'><div id='progress'></div></div>"+
-"<center><label id='dd' for='file-input'><br><br><br><br>déposez votre deck ici<input type='file' id='file-input'></label></center>"+
+"<center><label id='dd' for='file-input'><br><br><br><br>deposit your deck here<input type='file' id='file-input'></label></center>"+
 "<div id='com'><h1 id='comtitle'>コメント</h1><button onclick='closecom()'>X</button><br><span id='compar'></span><br><br></div>"+
 "<div id='memo'><h1>メモ</h1><button onclick='closememo()'>X</button><br><br><br><span id='memocont'></span></div>"+
 "<div id='cardId'>#<span id='cid'>1</span></div>"+
@@ -15,7 +15,7 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<br class='ansbox'><div id='answerbox2' onclick='jeu2()'>show answer</div>"+
 "<br class='ansbu'><div class='ansbu' id='ansbu1' onclick='jeu2check(1)'>correct</div><div class='ansbu' id='ansbu2' onclick='jeu2check(2)'>incorrect</div>"+
 "<div id='goodAnswer'>n/a</div>"+
-"<div id='combo'><span onclick='addmemo()' class='minmenu'>「ajouter au memo」</span><span onclick='showmemo()' class='minmenu'>「voir memo」</span><span class='minmenu' onclick='opencom()'>「commentaires」</span></div>";
+"<div id='combo'><span onclick='addmemo()' class='minmenu'>「add to memo」</span><span onclick='showmemo()' class='minmenu'>「show memo」</span><span class='minmenu' onclick='opencom()'>「comments」</span></div>";
 
 if(getCookie('jeu') == '1'){
   document.getElementById('answerbox').style.display = 'block';
--- pages/gestion.js
+++ pages/gestion.js
@@ -2,17 +2,17 @@ document.getElementsByTagName('body')[0].innerHTML +=""+
 "<div id='box2'>"+
 "<h1>Decks</h1>"+
 "<div id='bigrid'><div class='okButton jlptbt' title='Kotoba N5' onclick='updateJLPT(\"N5\")'>N5</div><div class='okButton allkana' onclick='updateJLPT(\"hiragana\")'>hiragana</div><div class='okButton jlptbt' title='Kotoba N4' onclick='updateJLPT(\"N4\")'>N4</div><div class='okButton allkana' onclick='updateJLPT(\"katakana\")'>katakana</div><div class='okButton jlptbt' title='Kotoba N3' onclick='updateJLPT(\"N3\")'>N3</div><div class='okButton allkana' id='kana' title='hiragana + katakana' onclick='updateJLPT(\"kana\")'>kana</div><div class='okButton jlptbt' title='Kotoba N2' onclick='updateJLPT(\"N2\")'>N2</div> <div class='okButton jlptbt' id='N1' title='Kotoba N1' onclick='updateJLPT(\"N1\")'>N1</div>"+
-"<div class='okButton' id='custom' title='glisser-déposer un deck créé avec Cardogus' onclick='goto(\"gather&dd=y\")'>deck personnel</div></div>"+
+"<div class='okButton' id='custom' title='glisser-déposer un deck créé avec Cardogus' onclick='goto(\"gather&dd=y\")'>personnal deck</div></div>"+
 "</div><div id='box2'>"+
 "<h1>Options</h1>"+
-"<br><br><div class='okButton okButton2' title='convertir un deck anki au format Cardogus' onclick='window.location=`convert.html`'>convertir</div>　"+
-"<div class='okButton okButton2' title='app pour création & gestion de decks' onclick='window.open(`cartory/index.html`,`_blank`)'>Gérer mes decks</div><br>"+
-"<h3>Style de révision :</h3>"+
+"<br><br><div class='okButton okButton2' title='convert an anki deck to Cardogus format' onclick='window.location=`convert.html`'>Convert</div>　"+
+"<div class='okButton okButton2' title='app pour création & gestion de decks' onclick='window.open(`cartory/index.html`,`_blank`)'>Manage decks</div><br>"+
+"<h3>Revision style :</h3>"+
 "<div id='kotoba' class='okButton okButton2' onclick='chooseGame(1)'>Kotoba</div> <div id='anki' class='okButton okButton2' onclick='chooseGame(2)'>Anki</div>"+
-"<h3>Sélection des questions :</h3>"+
-"<div id='fullrandom' class='okButton okButton2' onclick='updateFulldeck(1)'>Deck Entier Hasard</div> <div id='fullordered' class='okButton okButton2' onclick='updateFulldeck(2)'>Deck Entier Ordonné</div><br><br><div id='random' class='okButton okButton2' onclick='updateFulldeck(3)'>15 Aléatoires</div>"+
-"<div id='startid'><br><br><h2>Carte de départ</h2><input id='inputid' placeholder='"+getCookie('startnum')+"' type='text'></input> <button onclick='choosestart()'>ok</button></div><br>"+
-"<h3>Dictionnaire utilisé :</h3>"+
+"<h3>Questions selection :</h3>"+
+"<div id='fullrandom' class='okButton okButton2' onclick='updateFulldeck(1)'>Full Deck + Random</div> <div id='fullordered' class='okButton okButton2' onclick='updateFulldeck(2)'>Full Deck Ordered</div><br><br><div id='random' class='okButton okButton2' onclick='updateFulldeck(3)'>Cycle of 15 Random Cards</div>"+
+"<div id='startid'><br><br><h2>Card to start from</h2><input id='inputid' placeholder='"+getCookie('startnum')+"' type='text'></input> <button onclick='choosestart()'>ok</button></div><br>"+
+"<h3>Used dictionary :</h3>"+
 "<div id='dicojisho' class='okButton okButton2' onclick='setDico(\"jisho\")'>Jisho</div> <div id='dicojpdb' class='okButton okButton2' onclick='setDico(\"jpdb\")'>Jpdb</div> <div id='dicochinese' class='okButton okButton2' onclick='setDico(\"writtenchinese\")'>Writtenchinese</div><br><br>"+
 "</div></div>";
 checkrandomnessmenu();

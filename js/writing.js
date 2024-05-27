4912b62578f9da32b029e75c5df3a474f4287ced
kyuhubb <strylixeien@gmail.com>
2024-05-27 16:17:14 +0200

    update

--- js/writing.js
+++ js/writing.js
@@ -14,46 +14,114 @@ let currentKanji = -1; // Index of the current kanji
 let gradeLength = ""; // Length of the grade kanji list
 let ckanji = ""; // custom kanji list from url
 
-// Set the canvas line width
-ctx.lineWidth = 6;
+(function() {
+    
+    var sketch = document.getElementById('sketch');
+    var sketch_style = getComputedStyle(sketch);
+    canvas.width = parseInt(sketch_style.getPropertyValue('width'));
+    canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+    
+    
+    // Creating a tmp canvas
+    var tmp_canvas = document.createElement('canvas');
+    var tmp_ctx = tmp_canvas.getContext('2d');
+    tmp_canvas.id = 'tmp_canvas';
+    tmp_canvas.width = canvas.width;
+    tmp_canvas.height = canvas.height;
+    
+    sketch.appendChild(tmp_canvas);
 
-// Add event listeners
-canvas.addEventListener('pointerdown', startDrawing); // When mouse is pressed down on the canvas, start drawing
-canvas.addEventListener('pointermove', draw); // When mouse is moved on the canvas, draw
-canvas.addEventListener('pointerup', stopDrawing); // When mouse is released, stop drawing
-clearButton.addEventListener('click', clearCanvas); // When clear button is clicked, clear the canvas
-submitButton.addEventListener('click', submitCanvas); // When submit button is clicked, submit the canvas
-
-//write on canvas from the center of the cursor instead of top-left
-var cx = canvas.width * 0.001;
-var cy = canvas.height * 0.001;
-ctx.translate(cx, cy);
-
-// Initialize drawing state
-let drawing = false;
-
-// Function to start drawing
-function startDrawing(event) {
-    drawing = true;
-    ctx.beginPath();
-    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-}
-
-// Function to draw
-function draw(event) {
-    if (!drawing) return;
-    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-    ctx.stroke();
-}
-
-// Function to stop drawing
-function stopDrawing() {
-    drawing = false;
-}
+    var mouse = {x: 0, y: 0};
+    var last_mouse = {x: 0, y: 0};
+    
+    // Pencil Points
+    var ppts = [];
+    
+    /* Mouse Capturing Work */
+    tmp_canvas.addEventListener('pointermove', function(e) {
+        mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+        mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+    }, false);
+    
+    
+    /* Drawing on Paint App */
+    tmp_ctx.lineWidth = 6;
+    tmp_ctx.lineJoin = 'round';
+    tmp_ctx.lineCap = 'round';
+    tmp_ctx.strokeStyle = 'black';
+    tmp_ctx.fillStyle = 'black';
+    
+    tmp_canvas.addEventListener('pointerdown', function(e) {
+        tmp_canvas.addEventListener('pointermove', onPaint, false);
+        
+        mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
+        mouse.y = typeof e.offsetY !== 'undefined' ? e.offsetY : e.layerY;
+        
+        ppts.push({x: mouse.x, y: mouse.y});
+        
+        onPaint();
+    }, false);
+    
+    tmp_canvas.addEventListener('pointerup', function() {
+        tmp_canvas.removeEventListener('pointermove', onPaint, false);
+        
+        // Writing down to real canvas now
+        ctx.drawImage(tmp_canvas, 0, 0);
+        // Clearing tmp canvas
+        tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
+        
+        // Emptying up Pencil Points
+        ppts = [];
+    }, false);
+    
+    var onPaint = function() {
+        
+        // Saving all the points in an array
+        ppts.push({x: mouse.x, y: mouse.y});
+        
+        if (ppts.length < 3) {
+            var b = ppts[0];
+            tmp_ctx.beginPath();
+            //ctx.moveTo(b.x, b.y);
+            //ctx.lineTo(b.x+50, b.y+50);
+            tmp_ctx.arc(b.x, b.y, tmp_ctx.lineWidth / 2, 0, Math.PI * 2, !0);
+            tmp_ctx.fill();
+            tmp_ctx.closePath();
+            
+            return;
+        }
+        
+        // Tmp canvas is always cleared up before drawing.
+        tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
+        
+        tmp_ctx.beginPath();
+        tmp_ctx.moveTo(ppts[0].x, ppts[0].y);
+        
+        for (var i = 1; i < ppts.length - 2; i++) {
+            var c = (ppts[i].x + ppts[i + 1].x) / 2;
+            var d = (ppts[i].y + ppts[i + 1].y) / 2;
+            
+            tmp_ctx.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
+        }
+        
+        // For the last 2 points
+        tmp_ctx.quadraticCurveTo(
+            ppts[i].x,
+            ppts[i].y,
+            ppts[i + 1].x,
+            ppts[i + 1].y
+        );
+        tmp_ctx.stroke();
+        
+    };
+    
+}());
 
 // Function to clear the canvas
 function clearCanvas() {
+    var tmp_ctx = tmp_canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
 }
 
 // Function to submit the canvas
